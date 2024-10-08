bca6e8c6564e7ce44074fd78d64d9e608ab8d572
Caleb C <caleb.cushing@gmail.com>
2024-10-08 15:16:47 -0400

    refactor: use convention extension (#13)

--- buildSrc/src/main/kotlin/our.publish.gradle.kts
+++ buildSrc/src/main/kotlin/our.publish.gradle.kts
@@ -3,11 +3,8 @@
 
 plugins {
   id("com.gradle.plugin-publish")
-  // id("com.xenoterracide.gradle.convention.publish")
+  id("com.xenoterracide.gradle.convention.publish")
 }
-
-group = rootProject.group
-version = rootProject.version
 
 val repo = rootProject.name
 val username = "xenoterracide"
@@ -19,74 +16,7 @@ gradlePlugin {
   vcsUrl.set("${website.get()}.git")
 }
 
-publishing {
-  publications {
-    withType<MavenPublication>().configureEach {
-      logger.quiet("publication {} {}:{}:{}", name, groupId, artifactId, version)
-
-      versionMapping {
-        allVariants {
-          fromResolutionResult()
-        }
-      }
-
-      pom {
-        description = project.description
-        inceptionYear = "2024"
-        url = "$githubUrl/$repoShort"
-        licenses {
-          license {
-            name = "AGPL-3.0-or-later"
-            url = "https://choosealicense.com/licenses/agpl-3.0/"
-            distribution = "repo"
-            comments = "See README.md for more information."
-          }
-          license {
-            name = "Universal-FOSS-exception-1.0"
-            url = "https://spdx.org/licenses/Universal-FOSS-exception-1.0.html"
-            distribution = "repo"
-            comments = "See README.md for more information."
-          }
-          license {
-            name = "MIT"
-            url = "https://choosealicense.com/licenses/mit/"
-            distribution = "repo"
-            comments = "See README.md for more information."
-          }
-          license {
-            name = "CC-BY-4.0"
-            url = "https://choosealicense.com/licenses/cc-by-4.0/"
-            distribution = "repo"
-            comments = "See README.md for more information."
-          }
-          license {
-            name = "CC-BY-NC-4.0"
-            url = "https://creativecommons.org/licenses/by-nc/4.0/"
-            distribution = "repo"
-            comments = "See README.md for more information."
-          }
-        }
-        developers {
-          developer {
-            id = username
-            name = "Caleb Cushing"
-            email = "caleb.cushing@gmail.com"
-          }
-        }
-        scm {
-          connection = "$githubUrl/$repoShort.git"
-          developerConnection = "scm:git:$githubUrl/$repoShort.git"
-          url = "$githubUrl/$repoShort"
-        }
-      }
-    }
-  }
-
-  repositories {
-    maven {
-      name = "gh"
-      url = uri("https://maven.pkg.github.com/$repoShort")
-      credentials(PasswordCredentials::class)
-    }
-  }
+publicationLegal {
+  inceptionYear.set(2024)
+  spdxLicenseIdentifiers.add("Apache-2.0")
 }
--- module/publish/src/main/java/com/xenoterracide/gradle/convention/publish/PublicationLegalExtension.java
+++ module/publish/src/main/java/com/xenoterracide/gradle/convention/publish/PublicationLegalExtension.java
@@ -6,8 +6,21 @@ package com.xenoterracide.gradle.convention.publish;
 import org.gradle.api.provider.Property;
 import org.gradle.api.provider.SetProperty;
 
+/**
+ * Extension for configuring legal information regarding your project.
+ */
 public interface PublicationLegalExtension {
+  /**
+   * The year the project was started.
+   *
+   * @return inception year
+   */
   Property<Number> getInceptionYear();
 
+  /**
+   * SPDX license identifiers.
+   *
+   * @return all SPDX license identifiers
+   */
   SetProperty<String> getSpdxLicenseIdentifiers();
 }
