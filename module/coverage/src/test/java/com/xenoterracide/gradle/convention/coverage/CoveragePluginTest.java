74327e4d43d9412df3e855de8f588b5b1c43e21a
Caleb Cushing <caleb.cushing@gmail.com>
2024-10-09 10:45:16 -0400

    refactor: start testing coverage plugin

--- module/coverage/src/test/java/com/xenoterracide/gradle/convention/coverage/CoveragePluginTest.java
+++ module/coverage/src/test/java/com/xenoterracide/gradle/convention/coverage/CoveragePluginTest.java
@@ -3,21 +3,46 @@
 
 package com.xenoterracide.gradle.convention.coverage;
 
-import org.gradle.api.Project;
-import org.gradle.testfixtures.ProjectBuilder;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import org.gradle.testkit.runner.GradleRunner;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 
-class CoveragePluginTest {
+public class CoveragePluginTest {
 
-  Project project;
+  static final String GROOVY_SCRIPT =
+    """
+    plugins {
+      id("com.xenoterracide.gradle.convention.coverage")
+    }
+    """;
+
+  @TempDir
+  File testProjectDir;
 
   @BeforeEach
-  void setup() {
-    project = ProjectBuilder.builder().withName("that").build();
-    project.getPluginManager().apply(CoveragePlugin.class);
+  public void setupRunner() throws IOException {
+    Files.writeString(testProjectDir.toPath().resolve("settings.gradle"), "rootProject.name = 'hello-world'");
   }
 
   @Test
-  void apply() {}
+  void debug() throws IOException {
+    Files.writeString(testProjectDir.toPath().resolve("build.gradle"), GROOVY_SCRIPT);
+    var build = GradleRunner.create()
+      .withDebug(true)
+      .withProjectDir(testProjectDir)
+      .withArguments("check", "--stacktrace")
+      .withPluginClasspath()
+      .build();
+
+    assertThat(build.getOutput())
+      .contains("BUILD SUCCESSFUL")
+      .doesNotContain("Task :check UP-TO-DATE")
+      .doesNotContain("BUILD FAILED");
+  }
 }
