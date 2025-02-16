6eabcc779a6f02e51770891d937b4c0b217623b0
Caleb Cushing <caleb.cushing@gmail.com>
2025-02-15 21:03:17 -0500

    feat: create a checkstyle convention plugin

---
+++ module/checkstyle/src/main/java/com/xenoterracide/gradle/convention/checkstyle/package-info.java
@@ -0,0 +1,11 @@
+// SPDX-FileCopyrightText: Copyright © 2025 Caleb Cushing
+//
+// SPDX-License-Identifier: Apache-2.0
+
+/**
+ * Checkstyle convention plugin.
+ */
+@NullMarked
+package com.xenoterracide.gradle.convention.checkstyle;
+
+import org.jspecify.annotations.NullMarked;
---
+++ module/integration-test/checkstyle-integration-test/src/test/java/example/ExampleTest.java
@@ -0,0 +1,17 @@
+// SPDX-FileCopyrightText: Copyright © 2025 Caleb Cushing
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package example;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+
+class ExampleTest {
+
+  @Test
+  void test() {
+    assertNotNull(Example.class);
+  }
+}
