1224677e220de493f54406f7338f3100d07fc155
Caleb Cushing <caleb.cushing@gmail.com>
2026-02-09 21:10:19 -0500

    chore(license): update license headers to use `GPL-3.0-or-later WITH Classpath-exception-2.0`

--- module/integration-test/compile-integration-test/src/main/java/example/Example.java
+++ module/integration-test/compile-integration-test/src/main/java/example/Example.java
@@ -1,6 +1,5 @@
 // SPDX-FileCopyrightText: Copyright © 2026 Caleb Cushing
 //
-// SPDX-License-Identifier: Apache-2.0
 // SPDX-License-Identifier: GPL-3.0-or-later WITH Classpath-exception-2.0
 
 package example;
