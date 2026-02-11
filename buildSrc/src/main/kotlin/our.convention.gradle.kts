a709d2707e971988dc5eaa41b539e4d5812ef815
Caleb Cushing <caleb.cushing@gmail.com>
2026-02-11 16:31:57 -0500

    chore(buildSrc): enable convention javadoc plugin (#398)
    - add the convention javadoc plugin to our buildSrc convention script
    - ensure the plugin is brought in via the gradle lockfile update
    - re-enable runtime dependency on the convention javadoc artifact

--- buildSrc/src/main/kotlin/our.convention.gradle.kts
+++ buildSrc/src/main/kotlin/our.convention.gradle.kts
@@ -14,6 +14,7 @@ plugins {
   id("com.xenoterracide.gradle.convention.checkstyle")
   id("com.xenoterracide.gradle.convention.compile")
   id("com.xenoterracide.gradle.convention.coverage")
+  id("com.xenoterracide.gradle.convention.javadoc")
   id("com.xenoterracide.gradle.convention.publish")
   id("com.xenoterracide.gradle.convention.spotbugs")
 }
@@ -51,21 +52,4 @@ tasks.compileJava {
 
 mavenPublishing {
   configure(GradlePublishPlugin())
-}
-
-java {
-  withJavadocJar()
-  withSourcesJar()
-}
-tasks.withType<Javadoc>().configureEach {
-  dependsOn(tasks.classes)
-  source(sourceSets.main.map { it.output.generatedSourcesDirs })
-  (options as StandardJavadocDocletOptions).apply {
-    tags(
-      "apiSpec:a:API Spec:",
-      "apiNote:a:API Note:",
-      "implSpec:a:Implementation Spec:",
-      "implNote:a:Implementation Note:",
-    )
-  }
 }
